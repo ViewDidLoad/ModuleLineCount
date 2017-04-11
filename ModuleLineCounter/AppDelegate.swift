f425267efea917ed43bac2054ca3cb239b4750a7
ViewDidLoad <viewdidload@gmail.com>
2017-04-11 15:39:40 +0900

    Initial Commit

---
+++ ModuleLineCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModuleLineCounter
+//
+//  Created by viewdidload on 2017. 4. 11..
+//  Copyright © 2017년 ViewDidLoad. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ModuleLineCounter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModuleLineCounter
+//
+//  Created by viewdidload on 2017. 4. 11..
+//  Copyright © 2017년 ViewDidLoad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
